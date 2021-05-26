8d318a6108f1a14aca033ec8935e17be4caa2b81
dany gbelidji <dany.gbelidji@live.fr>
2021-05-26 09:12:31 +0200

    MOD - function on boiler plate

--- _boilerplate/src/functions.js
+++ _boilerplate/src/functions.js
@@ -1,5 +1,20 @@
 console.log('functions.js');
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random()*(max - min + 1) + min);
+export function setCurrency(value) {
+    return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(value)
+}
+
+export function setFormatTime(time) {
+    let mins = ~~(time / 60);
+    let secs = ~~(time - mins * 60);
+
+    let ret = "";
+
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
+
+export function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
